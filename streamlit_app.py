2f28e5468c81ab1cf4b9aa47c86f72344d08394b
Adrian Ramirez <105038581+testaramirez@users.noreply.github.com>
2022-05-09 13:27:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.text(my_data_row)
 
 #
 add_choice= streamlit.text_input('What fruit would you like to add?', 'Insert fruit')
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")                                
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES("+add_choice+")")                                
